3db63c256b86a90e059888093c86078900de94b0
v2Kamikaze <48386792+v2Kamikaze@users.noreply.github.com>
2022-02-09 09:37:52 -0300

    .

--- main.py
+++ main.py
@@ -137,7 +137,7 @@ plt.ylabel('accuracy')
 plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
 plt.show()
-# summarize history for loss
+
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
 plt.title('model loss')
